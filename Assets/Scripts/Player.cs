a6106454c18dd18d4810a1b4bc6ef49025b8dadd
IgorBrusko <i.brusko@maxfungames.com>
2023-09-12 19:21:58 +0700

    CreateProject

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+using System;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private CharacterController _characterController;
+    
+    [SerializeField] private int _speed = 1;
+    [SerializeField] private int _speed_rotation = 3;
+
+    void Update()
+    {
+        transform.Rotate(0, Input.GetAxis("Horizontal") * _speed_rotation, 0);
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        
+        float curSpeed = _speed * Input.GetAxis("Vertical") * (Input.GetKey(KeyCode.LeftShift) ? 3 : 1);
+
+        _characterController.SimpleMove(forward * curSpeed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.gameObject.name);
+    }
+}
---
+++ Assets/Scripts/Systems/EnemySpawnSystem.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemySpawnSystem : MonoBehaviour
+{
+    [SerializeField] private GameObject _player;
+    [SerializeField] private GameObject _testEnemyPrefab;
+    
+
+    private void Start()
+    {
+        StartCoroutine(SpawnEnemies());
+    }
+
+    private IEnumerator SpawnEnemies()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            var enemy = Instantiate(_testEnemyPrefab, GetRandomSpawnPosition(), Quaternion.identity);
+            var navMeshAgent = enemy.GetComponent<NavMeshAgent>();
+            navMeshAgent.SetDestination(_player.transform.position);
+            yield return new WaitForSeconds(1f);
+        }
+    }
+    
+    public Vector3 GetRandomSpawnPosition()
+    {
+        int x = Random.Range(0, 2) > 0 ? Random.Range(15, 17) : Random.Range(33, 35);
+        int z = Random.Range(0, 2) > 0 ? Random.Range(18, 20) : Random.Range(29, 33);
+        
+        Vector3 vec = Random.insideUnitCircle.normalized * 40;
+
+        var test = new Vector3(vec.x, vec.z, vec.y);
+        
+        return _player.transform.position + test;
+    }
+}
