52edef88b039627a2c45eba88f43b65e711359bf
IgorBrusko <i.brusko@maxfungames.com>
2023-09-28 19:56:30 +0700

    Начал делать авторизацию

--- Assets/Scripts/Launcher.cs
+++ Assets/Scripts/Launcher.cs
@@ -2,17 +2,41 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using DefaultNamespace;
 using Photon.Pun;
 using Photon.Realtime;
 using TMPro;
+using Unity.Services.Authentication;
+using Unity.Services.Core;
 using UnityEngine;
 using UnityEngine.UI;
+using AuthenticationException = System.Security.Authentication.AuthenticationException;
 
 public class Launcher : MonoBehaviourPunCallbacks
 {
     [SerializeField] private TMP_Text LogText;
     [SerializeField] private ToggleGroup _toggleGroup;
+    
+    [SerializeField] private TMP_Text _login;
+    [SerializeField] private TMP_Text _password;
+    
+    async void Awake()
+    {
+        try
+        {
+            await UnityServices.InitializeAsync();
+        }
+        catch (Exception e)
+        {
+            Debug.LogException(e);
+        }
+    }
+
+    public async void Register()
+    {
+        await SignUpWithUsernamePasswordAsync(_login.text, _password.text);
+    }
     
     void Start()
     {
@@ -22,6 +46,52 @@ public class Launcher : MonoBehaviourPunCallbacks
         PhotonNetwork.ConnectUsingSettings();
     }
 
+    // Setup authentication event handlers if desired
+    void SetupEvents() {
+        AuthenticationService.Instance.SignedIn += () => {
+            // Shows how to get a playerID
+            Debug.Log($"PlayerID: {AuthenticationService.Instance.PlayerId}");
+
+            // Shows how to get an access token
+            Debug.Log($"Access Token: {AuthenticationService.Instance.AccessToken}");
+
+        };
+
+        AuthenticationService.Instance.SignInFailed += (err) => {
+            Debug.LogError(err);
+        };
+
+        AuthenticationService.Instance.SignedOut += () => {
+            Debug.Log("Player signed out.");
+        };
+
+        AuthenticationService.Instance.Expired += () =>
+        {
+            Debug.Log("Player session could not be refreshed and expired.");
+        };
+    }
+    
+    async Task SignUpWithUsernamePasswordAsync(string username, string password)
+    {
+        try
+        {
+            await AuthenticationService.Instance.SignUpWithUsernamePasswordAsync(username, password);
+            Debug.Log("SignUp is successful.");
+        }
+        catch (AuthenticationException ex)
+        {
+            // Compare error code to AuthenticationErrorCodes
+            // Notify the player with the proper error message
+            Debug.LogException(ex);
+        }
+        catch (RequestFailedException ex)
+        {
+            // Compare error code to CommonErrorCodes
+            // Notify the player with the proper error message
+            Debug.LogException(ex);
+        }
+    }
+    
     public void CreateRoom()
     {
         PhotonNetwork.CreateRoom(null, new RoomOptions() { MaxPlayers = 2 });
