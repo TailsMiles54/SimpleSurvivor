c5da6279de09f4e582681314ccb675a34a24e74b
IgorBrusko <i.brusko@maxfungames.com>
2023-09-29 19:36:46 +0700

    Промежуточный

--- Assets/Scripts/AuthController.cs
+++ Assets/Scripts/AuthController.cs
@@ -6,11 +6,13 @@ using TMPro;
 using Unity.Services.Authentication;
 using Unity.Services.Core;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AuthController : MonoBehaviour
 {
     [SerializeField] private GameObject _authPanel;
     [SerializeField] private GameObject _connectPanel;
+    [SerializeField] private GameObject _characterPanel;
     
     [SerializeField] private TMP_InputField _login;
     [SerializeField] private TMP_InputField _password;
@@ -40,12 +42,18 @@ public class AuthController : MonoBehaviour
         await SignInWithUsernamePasswordAsync(_login.text, _password.text);
     }
 
+    public void Exit()
+    {
+        SignOutWithUsernamePasswordAsync();
+    }
+
     void SetupEvents() {
         AuthenticationService.Instance.SignedIn += () => {
             Debug.Log($"PlayerID: {AuthenticationService.Instance.PlayerId}");
             Debug.Log($"Access Token: {AuthenticationService.Instance.AccessToken}");
             _authPanel.SetActive(false);
-            _connectPanel.SetActive(true);
+            _connectPanel.SetActive(false);
+            _characterPanel.SetActive(true);
         };
 
         AuthenticationService.Instance.SignInFailed += (err) => {
@@ -53,6 +61,9 @@ public class AuthController : MonoBehaviour
         };
 
         AuthenticationService.Instance.SignedOut += () => {
+            _authPanel.SetActive(true);
+            _connectPanel.SetActive(false);
+            _characterPanel.SetActive(false);
             Debug.Log("Player signed out.");
         };
 
@@ -85,6 +96,22 @@ public class AuthController : MonoBehaviour
         {
             await AuthenticationService.Instance.SignInWithUsernamePasswordAsync(username, password);
             Debug.Log("SignIn is successful.");
+        }
+        catch (AuthenticationException ex)
+        {
+            Debug.LogException(ex);
+        }
+        catch (RequestFailedException ex)
+        {
+            Debug.LogException(ex);
+        }
+    }
+    
+    public void SignOutWithUsernamePasswordAsync()
+    {
+        try
+        {
+            AuthenticationService.Instance.SignOut();
         }
         catch (AuthenticationException ex)
         {
