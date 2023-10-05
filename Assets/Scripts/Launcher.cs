d2947b24a0ad9c461b2d5109286559ef19897a81
IgorBrusko <i.brusko@maxfungames.com>
2023-10-05 18:31:54 +0700

    Промежуточный переход по локациям

--- Assets/Scripts/Launcher.cs
+++ Assets/Scripts/Launcher.cs
@@ -1,20 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using ExitGames.Client.Photon;
 using Photon.Pun;
 using Photon.Realtime;
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 public class Launcher : MonoBehaviourPunCallbacks
 {
+    private static Launcher _instance;
+    public static Launcher Instance => _instance;
+    
     public async void SetupData()
     {
+        _instance = this;
         PhotonNetwork.NickName = await SaveDataManager.RetrieveSpecificData("character_name");
         PhotonNetwork.AutomaticallySyncScene = true;
         PhotonNetwork.GameVersion = "1";
         PhotonNetwork.ConnectUsingSettings();
+        DontDestroyOnLoad(gameObject);
     }
 
     public void CreateRoom()
     {
-        PhotonNetwork.CreateRoom(null, new RoomOptions() { MaxPlayers = 2 });
+        PhotonNetwork.CreateRoom(null, new RoomOptions
+        {
+            CustomRoomPropertiesForLobby = new string[]
+            {
+            },
+        });
     }
 
     public void JoinRoom() 
@@ -22,11 +37,34 @@ public class Launcher : MonoBehaviourPunCallbacks
         PhotonNetwork.JoinRandomRoom();
     }
 
+    [Button]
+    public void JoinOrCreateRoom(string mapName)
+    {
+        Dictionary<string, string> roomParams = new Dictionary<string, string>()
+        {
+            {"mapname", mapName}
+        };
+
+        Hashtable myHash = new Hashtable();
+
+        foreach (var param in roomParams)
+        {
+            myHash.Add(param.Key, param.Value);
+        }
+        
+        PhotonNetwork.JoinOrCreateRoom(Guid.NewGuid().ToString(), new RoomOptions()
+        {
+            CustomRoomProperties = myHash,
+        }, null, null);
+    }
+
     public override void OnJoinedRoom()
     {
         Log("Joined to room");
-        
-        PhotonNetwork.LoadLevel("SampleScene");
+
+        Debug.Log(PhotonNetwork.NetworkingClient.CurrentRoom.Name);
+        var mapname = PhotonNetwork.CurrentRoom.CustomProperties["mapname"].ToString();
+        PhotonNetwork.LoadLevel(mapname);
     }
 
     public override void OnConnectedToMaster()
